4e4b28d38631a5400726057f6fda092e1677b56e
nguyenduc11 <45514788+nguyenduc11@users.noreply.github.com>
2024-09-29 10:53:39 +0700

    add Add Product View Page

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -19,5 +19,26 @@ namespace MyApp.Controllers
             };
             return View(productViewModel);
         }
+
+        [HttpPost]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            // Check if the model state is valid
+            if (ModelState.IsValid)
+            {
+                // Add the product to the repository
+                ProductsRepository.AddProduct(productViewModel.Product);
+
+                // Redirect to the Index action after successful addition
+                return RedirectToAction(nameof(Index));
+            }
+            Console.WriteLine("ModelState is invalid");
+            // If the model state is invalid, repopulate the Categories list
+            productViewModel.Categories = CategoriesRepository.GetCategories();
+
+            // Return the view with the existing model to display validation messages
+            return View(productViewModel);
+        }
+
     }
 }
--- ViewModels/ProductViewModel.cs
+++ ViewModels/ProductViewModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using MyApp.Models;
 
 namespace MyApp.ViewModels
@@ -5,7 +6,6 @@ namespace MyApp.ViewModels
     public class ProductViewModel
     {
         public IEnumerable<Category> Categories { get; set; } = new List<Category>();
-
         public Product Product { get; set; } = new Product();
     }
 }
